4e4c0ad4e435c43f2452afc43b1e19f7d726569e
KrishnaSolo <krishna.solanki@mail.utoronto.ca>
2021-05-03 16:07:42 -0400

    removed unused import and added prod build

--- src/views/home.js
+++ src/views/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
+//import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/core/styles";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Box from "@material-ui/core/Box";
